1f52ad4fdafdea6a8690c7d3181a445b101683e4
alexvmt <alexandermerdiantarko@gmail.com>
2024-03-15 21:17:38 +0100

    update scripts

--- classified_image.js
+++ classified_image.js
@@ -22,7 +22,7 @@ var vis_params = {
 	};
 
 // Prediction
-var pred = ee.Image('projects/ee-alexvmt/assets/classified_image');
+var pred = ee.Image('projects/ee-alexvmt/assets/classified_image').clip(roi);
 var palette = [
   '419bdf', // water
   'a59b8f', // bare
@@ -35,6 +35,67 @@ var palette = [
 ];
 
 // Mapping
-Map.centerObject(roi, 8);
+Map.centerObject(roi, 9);
 Map.addLayer(s2_image, vis_params, 'Sentinel-2 RGB median composite 2023');
 Map.addLayer(pred, {min: 1, max: 8, palette: palette}, 'Land cover map');
+
+
+
+// set position of panel
+var legend = ui.Panel({
+  style: {
+    position: 'bottom-left',
+    padding: '8px 15px'
+  }
+});
+
+// Create legend title
+var legendTitle = ui.Label({
+  value: 'Legend',
+  style: {
+    fontWeight: 'bold',
+    fontSize: '18px',
+    margin: '0 0 4px 0',
+    padding: '0'
+    }
+});
+
+// Add the title to the panel
+legend.add(legendTitle);
+    
+// Creates and styles 1 row of the legend.
+var makeRow = function(color, name) {
+      
+      // Create the label that is actually the colored box.
+      var colorBox = ui.Label({
+        style: {
+          backgroundColor: '#' + color,
+          // Use padding to give the box height and width.
+          padding: '8px',
+          margin: '0 0 4px 0'
+        }
+      });
+      
+      // Create the label filled with the description text.
+      var description = ui.Label({
+        value: name,
+        style: {margin: '0 0 4px 6px'}
+      });
+      
+      // return the panel
+      return ui.Panel({
+        widgets: [colorBox, description],
+        layout: ui.Panel.Layout.Flow('horizontal')
+      });
+};
+
+// name of the legend
+var names = ['Water', 'Bare', 'Built up', 'Cropland', 'Grass', 'Shrub', 'Forest', 'Wetland'];
+
+// Add color and and names
+for (var i = 1; i < 8; i++) {
+  legend.add(makeRow(palette[i], names[i]));
+  }  
+
+// add legend to map (alternatively you can also print the legend to the console)  
+Map.add(legend);
--- raw_data.js
+++ raw_data.js
@@ -3,20 +3,47 @@ var roi  = ee.FeatureCollection('projects/ee-alexvmt/assets/Mufunta');
 var labeled_points = ee.FeatureCollection('projects/ee-alexvmt/assets/Mufunta-sample-data-POL_20240213');
 
 // set start and end date
-var start_date = ee.Date('2023-01-01');
-var end_date = ee.Date('2023-12-31');
+var start_date = '2023-01-01';
+var end_date = '2023-12-31';
 
-// create Sentinel-2 median composite for roi for specified date range with max 20% cloud cover and for selected bands
+// define relevant bands
 var s2_bands = ['B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B8A', 'B11', 'B12'];
-var s2_composite = ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED')
+
+// get Sentinel-2 imagery for roi for specified date range with max 20% cloud cover and for selected bands
+var s2_collection = ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED')
 	.filterBounds(roi)
 	.filterDate(start_date, end_date)
 	.filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 20))
-	.select(s2_bands)
-	.median();
+	.select(s2_bands);
+
+// rainy season
+var s2_composite_rainy_season_part_1 = s2_collection
+  .filterDate('2023-01-01', '2023-04-01');
+
+var s2_composite_rainy_season_part_2 = s2_collection
+  .filterDate('2023-11-15', '2024-01-01');
+	
+var s2_composite_rainy_season = s2_composite_rainy_season_part_1.merge(s2_composite_rainy_season_part_2)
+  .median()
+	.rename(['B2_rainy', 'B3_rainy', 'B4_rainy', 'B5_rainy', 'B6_rainy', 'B7_rainy', 'B8_rainy', 'B8A_rainy', 'B11_rainy', 'B12_rainy']);
+
+print(s2_composite_rainy_season.bandNames());
+
+// dry season
+var s2_composite_dry_season = s2_collection
+  .filterDate('2023-04-01', '2023-11-15')
+	.median()
+	.rename(['B2_dry', 'B3_dry', 'B4_dry', 'B5_dry', 'B6_dry', 'B7_dry', 'B8_dry', 'B8A_dry', 'B11_dry', 'B12_dry']);
+
+print(s2_composite_dry_season.bandNames());
+
+// combine data
+var combined_data = s2_composite_rainy_season.addBands([s2_composite_dry_season]);
+
+print(combined_data.bandNames());
 
 // sample data for labeled points
-var sampled_data = s2_composite.sampleRegions({
+var sampled_data = combined_data.sampleRegions({
 	collection: labeled_points,
 	properties: ['Landcover', 'LC_Out', 'LC_Nr'],
 	tileScale: 16,
